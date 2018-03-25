f2c41ef60cff50fcb7c679b8c90ef2487091f948
Fredrik Olofsson <evoozer@gmail.com>
2018-03-25 04:47:49 +0200

    index.mjs - Added head data

--- index.mjs
+++ index.mjs
@@ -1,4 +1,19 @@
 import Module from 'evoozer/Module';
 
+const setupRouter = ['routerProvider', routerProvider => {
+  routerProvider
+    .when('/', { Component: () => 'Hello world :-)' })
+    .otherwise({
+      head: {
+        // [type]: [string] => { innerHTML: [string] }
+        // [type]: [object]
+        title: 'Page not found'
+      },
+      http: { statusCode: 404 },
+      Component: () => 'Page not found'
+    });
+}];
+
 export default new Module()
-  .run(() => console.log('hello world'));
+  .config(setupRouter)
+  .run(() => console.log('hello world I am new here MOHAHAHA'));
